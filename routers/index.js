34c2ef50e06484d69a7a30f4ae2f02b8283597e3
SerenaFoo2 <serena_dango@hotmail.com>
2022-12-03 11:32:21 +0800

    updated

--- routers/index.js
+++ routers/index.js
@@ -1,43 +1,30 @@
 /*
     Routers are responsible to document APIs
 */
-
+const path = require("path");
 const express = require("express");
 const app = express();
-const {router: comedianRouter} = require("./comedian-router");
-const {router: showRouter} = require("./show-router");
-const {router: accountRouter} = require("./account-router");
-const {assign, unassign} = require('../controllers/show-controller');
-const {generatePayroll} = require("../services/payroll-service");
-const httpStatus = require("http-status");
-const {printSomethingMiddleware} = require("../middlewares/print-smt-middleware");
-const {myNameMiddleware} = require("../middlewares/my-name-middleware")
-const passport = require("passport"); // ADD
-const session = require("express-session"); // ADD
- 
-require("../config/passport")(passport); // ADD
+const {create, findAll, findOne, updateOne, deleteOne} = require("../controllers/comedian-controller");
+const {create: createShow, findAll: findAllShow, updateOne:updateOneShow, deleteOne:deleteOneShow} = require("../controllers/show-controller");
 
 // Middleware
 app.use(express.json());
-app.use(session({ secret: 'WDI-GENERAL-ASSEMBLY-EXPRESS' })); // ADD
-app.use(passport.initialize()); // ADD
-app.use(passport.session()); // ADD
-
-app.use("/comedians", [printSomethingMiddleware, myNameMiddleware]);
-app.use("/auth", accountRouter);
 
 // Endpoints
-app.use("/comedians", comedianRouter);
-app.use("/shows", showRouter);
-app.post("/assign/:showId/:comedianId", assign);
-app.delete("/assign/:showId/:comedianId", unassign);
-app.post("/generatepayroll/:month/:year", async (req, res) => {
+app.post("/comedians", create);
+app.get("/comedians", findAll);
+app.get("/comedians/:id", findOne);
+app.put("/comedians/:id", updateOne);
+app.delete("/comedians/:id", deleteOne);
 
-    const month = parseInt(req.params.month);
-    const year = parseInt(req.params.year);
-    await generatePayroll(month, year);
-    res.sendStatus(httpStatus.OK);
-})
+app.post("/shows", createShow);
+app.get("/shows", findAllShow);
+
+app.put("/shows/:id", updateOneShow);
+app.delete("/shows/:id", deleteOneShow);
+
+app.use(express.static("public")) // ADD THIS & create a folder "public"
+
 
 // Start server
 app.listen(3000, ()=>{
