5107ba1658f85ab8406fe3b28d4c2bb80ddfe8d6
SerenaFoo2 <serena_dango@hotmail.com>
2022-12-03 10:07:45 +0800

    init folder

--- routers/index.js
+++ routers/index.js
@@ -4,25 +4,40 @@
 
 const express = require("express");
 const app = express();
-const {create, findAll, findOne, updateOne, deleteOne} = require("../controllers/comedian-controller");
-const {create: createShow, findAll: findAllShow, updateOne:updateOneShow, deleteOne:deleteOneShow} = require("../controllers/show-controller");
+const {router: comedianRouter} = require("./comedian-router");
+const {router: showRouter} = require("./show-router");
+const {router: accountRouter} = require("./account-router");
+const {assign, unassign} = require('../controllers/show-controller');
+const {generatePayroll} = require("../services/payroll-service");
+const httpStatus = require("http-status");
+const {printSomethingMiddleware} = require("../middlewares/print-smt-middleware");
+const {myNameMiddleware} = require("../middlewares/my-name-middleware")
+const passport = require("passport"); // ADD
+const session = require("express-session"); // ADD
+ 
+require("../config/passport")(passport); // ADD
 
 // Middleware
 app.use(express.json());
+app.use(session({ secret: 'WDI-GENERAL-ASSEMBLY-EXPRESS' })); // ADD
+app.use(passport.initialize()); // ADD
+app.use(passport.session()); // ADD
+
+app.use("/comedians", [printSomethingMiddleware, myNameMiddleware]);
+app.use("/auth", accountRouter);
 
 // Endpoints
-app.post("/comedians", create);
-app.get("/comedians", findAll);
-app.get("/comedians/:id", findOne);
-app.put("/comedians/:id", updateOne);
-app.delete("/comedians/:id", deleteOne);
+app.use("/comedians", comedianRouter);
+app.use("/shows", showRouter);
+app.post("/assign/:showId/:comedianId", assign);
+app.delete("/assign/:showId/:comedianId", unassign);
+app.post("/generatepayroll/:month/:year", async (req, res) => {
 
-app.post("/shows", createShow);
-app.get("/shows", findAllShow);
-
-app.put("/shows/:id", updateOneShow);
-app.delete("/shows/:id", deleteOneShow);
-
+    const month = parseInt(req.params.month);
+    const year = parseInt(req.params.year);
+    await generatePayroll(month, year);
+    res.sendStatus(httpStatus.OK);
+})
 
 // Start server
 app.listen(3000, ()=>{
